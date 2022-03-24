b33f8f78f57ab609c8e3c14a19850ee69c8791f7
YojiSato65 <yojisato65@gmail.com>
2022-03-24 17:33:11 +0200

    deleted node_module

--- m4-d7/src/components/Registration.jsx
+++ m4-d7/src/components/Registration.jsx
@@ -1,22 +1,63 @@
+import { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 
 const Registration = () => {
+  const [registration, setRagistration] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  // const [name, setName] = useState('')
+  // const [surname, setSurname] = useState('')
+  // const [email, setEmail] = useState('')
+  // const [password, setPassword] = useState('')
+  // const [confirmPassword, setConfirmPassword] = useState('')
+
+  //   const emailValidation = () =>
+  //   {
+  //     const validEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  //     if (email == validEmail)
+  //     {
+
+  //     } else
+  //     {
+
+  //   }
+  // }
+
   return (
     <Form>
-      <Form.Group controlId="formBasicEmail">
+      <Form.Group>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter name"
+          required
+          minlength={2}
+          value="registration.name"
+          onchange={(e) =>
+            setRagistration({
+              ...registration,
+              name: e.target.value,
+            })
+          }
+        />
+        <Form.Label>Surname</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter surname"
+          required
+          minlength={3}
+        />
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Confirm your password" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
